cba9b74541e1824dbdfe3ccbfb5331c73f6d4845
jonatacabral <jonatajul@gmail.com>
2025-11-02 22:51:23 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Exemplo de template SPA
+document.addEventListener('DOMContentLoaded', function(){
+    const main = document.getElementById('content');
+    const template = (titulo, texto) => `<section><h2>${titulo}</h2><p>${texto}</p></section>`;
+    
+    
+});
